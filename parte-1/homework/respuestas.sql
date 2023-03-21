93800ac6d54686ab4f573700815e4550bd88ecb1
YesaGGG <128443702+YesaGGG@users.noreply.github.com>
2023-03-20 22:13:01 -0600

    Update respuestas.sql

--- parte-1/homework/respuestas.sql
+++ parte-1/homework/respuestas.sql
@@ -1,7 +1,54 @@
---1
-select * from stg.product_master
+--1)
+select *                                                                                                                                                                         
+from stg.product_master 
 where categoria = 'Electro'
-
---2
-select * from stg.product_master
-where origen = 'China'
+--2)
+select * 
+from stg.product_master 
+where origen= 'China'
+--3) 
+select *
+from stg.product_master
+where categoria = 'Electro'
+order by nombre
+--4)
+select *
+from stg.product_master
+where categoria = 'Electro'
+order by nombre
+--5) 
+select *
+from stg.store_master
+where pais = 'Argentina'
+order by fecha_apertura
+--6) 
+select*
+from stg.order_line_sale
+order by fecha desc
+limit 5
+--7)
+ select*
+from stg.super_store_count
+order by fecha
+limit 10
+--8) 
+select*
+from stg.product_master
+where categoria = 'Electro' and 
+subsubcategoria not in ('Soporte','Control remoto')
+--9)
+select*
+from stg.order_line_sale
+where moneda = 'ARS' and venta > 100.000
+--10) 
+select*
+from stg.order_line_sale
+where fecha between '2022-10-01' and '2022-10-31'
+--11) 
+select*
+from stg.product_master
+where ean is not null
+--12)  
+select*
+from stg.order_line_sale
+where fecha between '2022-10-01' and '2022-11-10'
